3c0d8fe2c242b0ab4c9c709cb61a70a9a3ed59ea
Yudhvir Raj <rajyudhvir29@gmail.com>
2017-04-17 14:57:13 -0700

    SQL
    Sql file to create user

---
+++ create_note-to-myself_use.sql
@@ -0,0 +1,3 @@
+CREATE USER 'notetomyself'@'localhost' Identified by '1234';
+GRANT ALL ON notetomyself.* TO 'notetomyself'@'localhost';
+-- GRANT ALL PRIVILEGES ON notetomyself.* To 'notetomyself@localhost' identified BY '1234'
